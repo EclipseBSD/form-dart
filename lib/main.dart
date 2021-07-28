638a70c9792251565bc76ce4dbfa03a17c81744a
Saturn <viniciusmarques_2020@outlook.com>
2021-07-28 00:34:45 -0400

    Adicionei um print no código.

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ void main() {
 }
 
 class Formulario extends StatelessWidget {
-
   //Criação dos controladores
 
   final TextEditingController _controladorNome = TextEditingController();
@@ -21,55 +20,58 @@ class Formulario extends StatelessWidget {
 
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Cadastrando produto")),
-        body: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.only(top: 8.0),
-                child: TextField(
-                  controller: _controladorNome,
-                  decoration: InputDecoration(labelText: "Nome"),
+          appBar: AppBar(title: Text("Cadastrando produto")),
+          body: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 8.0),
+                  child: TextField(
+                    controller: _controladorNome,
+                    decoration: InputDecoration(labelText: "Nome"),
+                  ),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 16.0),
-                child: TextField(
-                  controller: _controladorQuantidade,
-                  decoration: InputDecoration(labelText: "Quantidade"),
-                  keyboardType: TextInputType.number,
+                Padding(
+                  padding: EdgeInsets.only(top: 16.0),
+                  child: TextField(
+                    controller: _controladorQuantidade,
+                    decoration: InputDecoration(labelText: "Quantidade"),
+                    keyboardType: TextInputType.number,
+                  ),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 16.0),
-                child: TextField(
-                  controller: _controladorValor,
-                  decoration: InputDecoration(labelText: "Valor"),
-                  keyboardType: TextInputType.number,
+                Padding(
+                  padding: EdgeInsets.only(top: 16.0),
+                  child: TextField(
+                    controller: _controladorValor,
+                    decoration: InputDecoration(labelText: "Valor"),
+                    keyboardType: TextInputType.number,
+                  ),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 16.0),
-                child: ElevatedButton(
-                  child: Text("Cadastrar"),
+                Padding(
+                  padding: EdgeInsets.only(top: 16.0),
+                  child: ElevatedButton(
+                    child: Text("Cadastrar"),
 
-                  //Configurando os controladores para reconhecer no formulário.
+                    //Configurando os controladores para reconhecer no formulário.
 
-                  onPressed: () {
-                    final String nome = _controladorNome.text;
-                    final int? quantidade = int.tryParse(_controladorQuantidade.text);
-                    final double? valor = double.tryParse(_controladorValor.text);
+                    onPressed: () {
+                      final String nome = _controladorNome.text;
+                      final int? quantidade =
+                          int.tryParse(_controladorQuantidade.text);
+                      final double? valor =
+                          double.tryParse(_controladorValor.text);
 
-                    final Produto produtoNovo = Produto(nome, quantidade!, valor!);
-                    print(produtoNovo);
-                  },
-                ),
-              )
-            ],
-          ),
-        )
-      ),
+                      final Produto produtoNovo =
+                          Produto(nome, quantidade!, valor!);
+                      print(produtoNovo);
+                      print(nome);
+                    },
+                  ),
+                )
+              ],
+            ),
+          )),
     );
   }
 }
